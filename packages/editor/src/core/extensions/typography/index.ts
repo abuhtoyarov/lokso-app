acfbe4a1a12e5217692d57bc16607ed2ac94f0ab
Bukhtoiarov Artem <baa@r2d2.dev>
2026-07-25 22:09:32 +0300

    chore(editor): add missing license headers to typography extension files
    copyright-check.yml matches "Copyright" case-sensitively; these two files
    were missed by an earlier case-insensitive grep that matched the
    copyright: input-rule identifier instead of a license header.

--- packages/editor/src/core/extensions/typography/index.ts
+++ packages/editor/src/core/extensions/typography/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2023-present Plane Software, Inc. and contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * See the LICENSE file for details.
+ */
 import type { InputRule } from "@tiptap/core";
 import { Extension } from "@tiptap/core";
 // constants
